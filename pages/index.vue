415f35dac084101a7f0d2f0a12184bf6454d4465
husainsalah <husain.salah@gmail.com>
2024-01-11 17:19:22 -0500

    post local build

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,8 @@
 <template>
   <div>
     <div class="relative flex items-center justify-center h-fit overflow-none">
-      <video
-        playsinline
-        autoplay
-        loop
-        muted
-        class="fixed top-0 z-10 max-w-none w-full object-display-cover h-fit"
-      >
-        <source
-          src="~/images/bags/bags.jpg"
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
       <div class="relative justify-center mt-60 font-hairline overflow-none">
+        <img src="~/public/images/bags/bags.jpg" />
         <div
           class="relative w-full z-20 p-5 text-2xl border-white border-2 text-center text-2xl bg-white bg-opacity-60 transition-all animate-pulse hover:bg-white hover:bg-opacity-80 hover:scale-105 hover:animate-none"
         >
@@ -31,9 +19,8 @@
 <script setup lang="ts">
   // import { NGradientText } from 'naive-ui'
 </script>
-
 <style scoped>
-  video {
+  img {
     position: fixed;
     top: 0;
     left: 0;
